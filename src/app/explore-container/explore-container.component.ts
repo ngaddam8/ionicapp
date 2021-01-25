3711136efb4d80cfa2aab31d8df9a64eed29c7d9
Ehtisham Hussain Yasin <ehtisham@xlr8solutions.com>
2021-01-25 15:09:15 +0000

    repo init

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { AlertController } from "@ionic/angular";
 
 @Component({
   selector: 'app-explore-container',
@@ -8,7 +9,41 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ExploreContainerComponent implements OnInit {
   @Input() name: string;
 
-  constructor() { }
+  private AlertMessage: string;
+
+  constructor(public alertController: AlertController) { }
+
+  async openAlert(event){
+    
+    this.AlertMessage = "Are you shure you want to create a new user?";
+    let that = this;
+
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'New User',
+      message: that.AlertMessage,
+      buttons: [
+        {
+          text: "Yes",
+          handler: function(){
+            console.log("Yes is clicked");
+          }
+        }, 'Cancel']
+    });
+
+    this.test((data)=>{
+      console.log("The callback for the called function");
+      console.log(data);
+    })
+
+    await alert.present();
+  }
+
+
+  test(callback){
+    console.log("this is the called function");
+    callback({'status': 'success', 'message': '200'});
+  }
 
   ngOnInit() {}
 
